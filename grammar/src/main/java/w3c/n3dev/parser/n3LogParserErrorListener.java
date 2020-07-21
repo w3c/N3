a6a5a6236181fd5054a39e8fc4eff27b9a42d936
William Van Woensel <william.van.woensel@gmail.com>
2020-07-20 21:10:55 -0300

    printing full-context, ambiguity errors as well

--- grammar/src/main/java/w3c/n3dev/parser/n3LogParserErrorListener.java
+++ grammar/src/main/java/w3c/n3dev/parser/n3LogParserErrorListener.java
@@ -1,7 +1,12 @@
 package w3c.n3dev.parser;
 
+import java.util.BitSet;
+
+import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
 
 import wvw.utils.log.Log;
 
@@ -27,5 +32,33 @@ public class n3LogParserErrorListener extends n3ParserErrorListener {
 
 		Log.e(name + " [parser]: prefixError: " + e.getMessage() + " (\"" + offendingPrefix + "\") in \"" + context
 				+ "\"");
+	}
+
+	@Override
+	public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact,
+			BitSet ambigAlts, ATNConfigSet configs) {
+
+		onError();
+
+		Log.e(name + "[parser]: " + "reportAmbiguity @" + startIndex + "-" + stopIndex + ", exact: " + exact
+				+ ", ambigAlts: " + ambigAlts); // + ", configs: " + configs);
+	}
+
+	@Override
+	public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex,
+			BitSet conflictingAlts, ATNConfigSet configs) {
+
+		onError();
+		Log.e(name + "[parser]: " + "reportAttemptingFullContext @" + startIndex + "-" + stopIndex
+				+ ", conflictingAlts: " + conflictingAlts); // + ", configs " + configs);
+	}
+
+	@Override
+	public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction,
+			ATNConfigSet configs) {
+
+		onError();
+		Log.e(name + "[lexer]: " + "reportContextSensitivity @" + startIndex + "-" + stopIndex + ", prediction: "
+				+ prediction); // + ", configs: " + configs);
 	}
 }
